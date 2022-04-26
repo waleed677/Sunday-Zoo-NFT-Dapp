44377998c4fe10ec2c5abf7408358b4d6ddf20e2
waleed677 <waleedmalik677@gmail.com>
2022-04-26 23:28:48 +0500

    mint section brings to center

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -125,7 +125,7 @@ export const Mint = styled.div`
   padding: 10px 40px;
   border-radius: 25px;
   width: 30%;
-  margin-top: 13vh;
+  margin-top: 25vh;
   background: rgba(55, 110, 255, 0.9);
   // box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
